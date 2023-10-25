7a488c15c68fcf0b4d1d7a836277bf43ec0b75d8
iSaklov <ivansaklov@gmail.com>
2023-10-25 16:13:28 +0200

    refactor: updates airtable variables

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import { ItemsProvider } from '../context/items'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<ItemsProvider>
+			<Component {...pageProps} />
+		</ItemsProvider>
+	)
 }
 
 export default MyApp
